6f9e0745781f8adb5ad1648e1730c5f59ab0664f
rizco renova <rizcorenova31@gmail.com>
2022-10-04 20:32:51 +0700

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -20,9 +20,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "2b9bdfcb2e2e8856618be9b9189a635e",
-"/": "2b9bdfcb2e2e8856618be9b9189a635e",
-"main.dart.js": "c0ee0fbb5d8d098121f12cd1a954b117",
+"index.html": "fccffbfc0a9e24fca749446ff6efa998",
+"/": "fccffbfc0a9e24fca749446ff6efa998",
+"main.dart.js": "354f24e17cfe0717d9cc810086e87666",
 "manifest.json": "a54cf99c003cc45b9d007395498bbea4",
 "version.json": "39bef40ce107224d5cd3a6ed51747926"
 };
